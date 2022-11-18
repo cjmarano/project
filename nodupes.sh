0fcc181e2b758b6f5783fc462570e2a7b57b370a
cjmarano <109162755+cjmarano@users.noreply.github.com>
2022-11-18 08:01:34 -0500

    synch round to big

---
+++ nodupes.sh
@@ -0,0 +1,2 @@
+#!sh
+export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/local/jamf/bin:/usr/bin:/bin:/usr/sbin:/sbin:~/.pyenv/versions/3.10.3/bin:~/.pyenv/shims/python
---
+++ score.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//test codecdc
+
+int main(void)
+
+{
+  
+    int score1 = 72;
+    int score2 = 73;
+    int score3 = 33;
+
+    printf("Average: %f\n", (score1 + score2 + score3) / 3.0);
+
+}
