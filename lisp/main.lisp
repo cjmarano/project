b3d08f2b7b7969fcc8e0f0c1f1af39f38341eb63
cjmarano <109162755+cjmarano@users.noreply.github.com>
2023-06-02 08:44:19 -0400

    Moved lisp files into project

---
+++ lisp/main.lisp
@@ -0,0 +1,6 @@
+#!/usr/local/bin/clisp
+
+(setf x (make-array '(3 3)
+		    :initial-contents '((0 1 2  ) (3 4 5) (6 7 8)))
+      )
+(write x)
---
+++ lisp/sequence.lisp
@@ -0,0 +1,32 @@
+(setq x (vector 'a 'b 'c 'a 'b 'c 'd 'e 'f 'a))
+(write (length x))
+(terpri)
+(write (elt x 0))
+(terpri)
+(write (elt x 1))
+(terpri)
+(write (elt x 2))
+(terpri)
+(write (elt x 3))
+(terpri)
+(write (elt x 4))
+(terpri)
+(write (elt x 5))
+(terpri)
+(write (elt x 6))
+(terpri)
+(write (elt x 7))
+(terpri)
+(write (elt x 8))
+(terpri)
+(write (elt x 9))
+(terpri)
+(write (count 9 '(0 1 2 3 4 5 6 7 9 9)))
+
+
+
+
+
+
+
+
