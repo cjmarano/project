9d84a8acabda2f4899dfc81cbe581201b6295c74
cjmarano <109162755+cjmarano@users.noreply.github.com>
2025-09-04 07:39:31 -0400

    synch init.el Big <-> New.

--- BIGinit.el
+++ BIGinit.el
@@ -31,6 +31,17 @@
 
          ))
 
+(add-hook 'emacs-startup-hook
+      (lambda ()
+        "Restore defalut values after init."
+        (setq gc-cons-threshold 800000)))
+
+;; (use-package benchmark-init
+;;   :ensure t
+;;   :config
+;;   ;; To disable collection of benchmark data after init is done.
+;;   (add-hook 'after-init-hook 'benchmark-init/deactivate))
+
 (setq load-prefer-newer t)
 (add-to-list 'load-path "~/.emacs.d/elpa/auto-compile-20250901.1341")
 (require 'auto-compile)
@@ -623,23 +634,11 @@
  '(company-box-icons-alist 'company-box-icons-images)
  '(custom-enabled-themes '(sanityinc-tomorrow-eighties))
  '(custom-safe-themes
-   '("7771c8496c10162220af0ca7b7e61459cb42d18c35ce272a63461c0fc1336015"
-     "e8bd9bbf6506afca133125b0be48b1f033b1c8647c628652ab7a2fe065c10ef0"
-     "c5975101a4597094704ee78f89fb9ad872f965a84fb52d3e01b9102168e8dc40"
-     "7235b77f371f46cbfae9271dce65f5017b61ec1c8687a90ff30c6db281bfd6b7"
-     "b4b5da90759ab14719f1e1d8d0138ff72d2901e8a63748b172944627513cfffb"
-     "ba4f725d8e906551cfab8c5f67e71339f60fac11a8815f51051ddb8409ea6e5c"
+   '("ba4f725d8e906551cfab8c5f67e71339f60fac11a8815f51051ddb8409ea6e5c"
      "ad7d874d137291e09fe2963babc33d381d087fa14928cb9d34350b67b6556b6d"
      "2721b06afaf1769ef63f942bf3e977f208f517b187f2526f0e57c1bd4a000350"
      "04aa1c3ccaee1cc2b93b246c6fbcd597f7e6832a97aaeac7e5891e6863236f9f"
      default))
- '(dashboard-startupify-list
-   '(dashboard-insert-banner dashboard-insert-newline
-                             dashboard-insert-banner-title
-                             dashboard-insert-newline
-                             dashboard-insert-init-info
-                             dashboard-insert-items
-                             dashboard-insert-newline))
  '(denote-known-keywords '("emacs" "init" "general" "testing"))
  '(flycheck-python-flake8-executable "python3")
  '(flycheck-python-pycompile-executable "python3")
