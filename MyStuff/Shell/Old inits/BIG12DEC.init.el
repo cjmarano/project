0400ac99d74129cdbb2e2bd867f9e1fc6f9b53f9
cjmarano <109162755+cjmarano@users.noreply.github.com>
2025-12-12 10:29:16 -0500

    FUCKEDY FUCK FUCK

--- MyStuff/Shell/Old inits/BIG12DEC.init.el
+++ MyStuff/Shell/Old inits/BIG12DEC.init.el
@@ -47,7 +47,7 @@
 (setq auto-save-file-name-transforms
       `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
 
-(add-to-list 'default-frame-alist '(height . 28))
+(add-to-list 'default-frame-alist '(height . 60))
 (add-to-list 'default-frame-alist '(width . 110))
 
 (server-start)
