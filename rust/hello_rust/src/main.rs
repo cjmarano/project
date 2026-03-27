95c31b922f85c9a16bfd5d452484da8907a94c17
cjmarano <109162755+cjmarano@users.noreply.github.com>
2026-03-27 10:27:46 -0400

    Small changes to a rust file generate a LOT of additional changes.

--- rust/hello_rust/src/main.rs
+++ rust/hello_rust/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let stdout = stdout();
     let message = String::from("Hello fellow Rustaceans!");
     let width = message.chars().count();
-
+    let length: String = String::from("Hello Folks"):
     let mut writer = BufWriter::new(stdout.lock());
     say(&message, width, &mut writer).unwrap();
 }
