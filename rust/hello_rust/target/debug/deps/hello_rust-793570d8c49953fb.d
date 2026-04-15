3b71373725bd6fcc352d83f77785e2877dd5ad30
cjmarano <109162755+cjmarano@users.noreply.github.com>
2026-04-15 13:40:34 -0400

    Rust generates so much noise

---
+++ rust/hello_rust/target/debug/deps/hello_rust-793570d8c49953fb.d
@@ -0,0 +1,5 @@
+/Users/charles.marano/project/rust/hello_rust/target/debug/deps/hello_rust-793570d8c49953fb.d: src/main.rs
+
+/Users/charles.marano/project/rust/hello_rust/target/debug/deps/libhello_rust-793570d8c49953fb.rmeta: src/main.rs
+
+src/main.rs:
---
+++ rust/hello_rust/target/debug/deps/hello_rust-e30747848735800d.d
@@ -0,0 +1,5 @@
+/Users/charles.marano/project/rust/hello_rust/target/debug/deps/hello_rust-e30747848735800d.d: src/main.rs
+
+/Users/charles.marano/project/rust/hello_rust/target/debug/deps/libhello_rust-e30747848735800d.rmeta: src/main.rs
+
+src/main.rs:
