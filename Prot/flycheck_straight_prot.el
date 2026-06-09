c13c17ca4e61c1bebaa61eb2f1e3d7c3cc9aea5d
cjmarano <109162755+cjmarano@users.noreply.github.com>
2026-06-09 11:20:19 -0400

    Version of Prot init.el that uses straight.

---
+++ Prot/flycheck_straight_prot.el
@@ -0,0 +1,151 @@
+;;; Set up the package manager
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; straight compiles or native compiles all packages by default.
+;; (setq package-native-compile t)
+
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
+
+(add-to-list 'exec-path "/Users/charles.marano/.pyenv/shims/")
+
+;; changed from ~/.cache/emacs
+(setq user-emacs-directory "~/.emacs.d/")
+
+
+
+(add-to-list 'display-buffer-alist
+             '("\\`\\*\\(Warnings\\|Compile-Log\\)\\*\\'"
+               (display-buffer-no-window)
+               (allow-no-window . t)))
+
+;;; Tweak the looks of Emacs
+ 
+;; Those three belong in the early-init.el, but I am putting them here
+;; for convenience.  If the early-init.el exists in the same directory
+;; as the init.el, then Emacs will read+evaluate it before moving to
+;; the init.el.
+(menu-bar-mode 1)
+(tool-bar-mode -1)
+
+;; part after :family is wrong for default, fixed-pitch 
+;; (let ((mono-spaced-font "Hack Nerd Font Mono")
+;;       (proportionately-spaced-font "FiraCode Nerd Font"))
+;;   (set-face-attribute 'default nil :family Hack Nerd Font Mono :height 100)
+;;   (set-face-attribute 'fixed-pitch nil :family Hack Nerd Font Mono :height 1.0)
+;;   (set-face-attribute 'variable-pitch nil :family Hack Nerd Font Propo :height 1.0))
+
+;; Remember to do M-x and run `nerd-icons-install-fonts' to get the
+;; font files.  Then restart Emacs to see the effect.
+(use-package nerd-icons
+  :ensure t)
+
+(use-package nerd-icons-completion
+  :ensure t
+  :after marginalia
+  :config
+  (add-hook 'marginalia-mode-hook #'nerd-icons-completion-marginalia-setup))
+
+;; (use-package nerd-icons-dired
+;;   :ensure t
+;;   :hook
+;;   (dired-mode . nerd-icons-dired-mode))
+
+;;; Configure the minibuffer and completions
+
+(use-package vertico
+  :ensure t
+  :hook (after-init . vertico-mode))
+
+(use-package marginalia
+  :ensure t
+  :hook (after-init . marginalia-mode))
+
+(use-package orderless
+  :ensure t
+  :config
+  (setq completion-styles '(orderless basic))
+  (setq completion-category-defaults nil)
+  (setq completion-category-overrides nil))
+
+;;; The file manager (Dired)
+
+;; (use-package dired
+;;   :ensure nil
+;;   :commands (dired)
+;;   :hook
+;;   ((dired-mode . dired-hide-details-mode)
+;;    (dired-mode . hl-line-mode))
+;;   :config
+;;   (setq dired-recursive-copies 'always)
+;;   (setq dired-recursive-deletes 'always)
+;;   (setq delete-by-moving-to-trash t)
+;;   (setq dired-dwim-target t))
+
+;; (use-package dired-subtree
+;;   :ensure t
+;;   :after dired
+;;   :bind
+;;   ( :map dired-mode-map
+;;     ("<tab>" . dired-subtree-toggle)
+;;     ("TAB" . dired-subtree-toggle)
+;;     ("<backtab>" . dired-subtree-remove)
+;;     ("S-TAB" . dired-subtree-remove))
+;;   :config
+;;   (setq dired-subtree-use-backgrounds nil))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(backup-directory-alist '(("" . "~/.backups")))
+ '(company-box-enable-icon t)
+ '(company-box-icons-alist 'company-box-icons-images)
+ '(custom-enabled-themes '(sanityinc-tomorrow-eighties))
+ '(custom-safe-themes
+   '("ba4f725d8e906551cfab8c5f67e71339f60fac11a8815f51051ddb8409ea6e5c"
+     "ad7d874d137291e09fe2963babc33d381d087fa14928cb9d34350b67b6556b6d"
+     "2721b06afaf1769ef63f942bf3e977f208f517b187f2526f0e57c1bd4a000350"
+     "04aa1c3ccaee1cc2b93b246c6fbcd597f7e6832a97aaeac7e5891e6863236f9f"
+     default))
+ '(dashboard-startupify-list
+   '(dashboard-insert-banner dashboard-insert-newline
+                             dashboard-insert-banner-title
+                             dashboard-insert-newline
+                             dashboard-insert-init-info
+                             dashboard-insert-items
+                             dashboard-insert-newline))
+ '(denote-known-keywords '("emacs" "init" "general" "testing"))
+ '(flycheck-python-flake8-executable "python3")
+ '(flycheck-python-pycompile-executable "python3")
+ '(flycheck-python-pylint-executable "python3")
+ '(org-agenda-files '("~/project/org/3.org"))
+ '(org-faces-easy-properties
+   '((todo . :background) (tag . :foreground) (priority . :foreground)))
+ '(org-id-locations-file "$HOME/.cache/emacs/var/org/id-locations.el" t)
+ '(org-startup-folded 'fold)
+ '(org-tempo-keywords-alist nil)
+ '(sort-fold-case t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:height 180 :family "JetBrainsMono Nerd Font"))))
+ '(cursor ((t (:background "light green"))))
+ '(org-headline-done ((t (:foreground "gray80"))))
+ '(org-level-1 ((t (:inherit outline-1 :background "gray22" :box (:line-width (1 . 1) :style released-button) :weight bold :height 1.3))))
+ '(org-level-2 ((t (:inherit outline-2 :background "gray23" :box (:line-width (1 . 1) :style released-button) :height 1.2)))))
+
